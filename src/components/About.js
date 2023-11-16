25b19f3dbe47662f764f6eeaa0b98605529c1f0d
Betojr04 <betomasia12@gmail.com>
2023-11-15 22:39:03 -0700

    added images to the assets folder in src to have pics of me

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/About.css";
+import photo from "../assets/photo.jpeg";
 
 export const About = () => {
   return (
     <section class="about-section" id="about">
       <div class="about-image">
         <img
-          src="https://images.unsplash.com/photo-1549692520-acc6669e2f0c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZGV2ZWxvcGVyfGVufDB8MXwwfHx8MA%3D%3D"
+          // src="https://images.unsplash.com/photo-1549692520-acc6669e2f0c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZGV2ZWxvcGVyfGVufDB8MXwwfHx8MA%3D%3D"
+          src={photo}
           alt="About Image"
         />
       </div>
