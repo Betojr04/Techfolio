ccb6059f94a85ed63539d4d221015219e3e4ba8a
Betojr04 <betomasia12@gmail.com>
2023-11-16 15:46:38 -0700

    changed the logo next to my name in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faConnectdevelop } from "@fortawesome/free-brands-svg-icons";
 
 export const Navbar = () => {
   return (
     <nav class="navbar">
       <div class="navbar-brand">
         <Link to="/">
-          Beto <span class="icon">🌟</span>
+          Beto <FontAwesomeIcon icon={faConnectdevelop} />
         </Link>
       </div>
       <ul class="navbar-nav">
