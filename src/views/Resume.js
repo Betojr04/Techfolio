1d84b7390972c893e74f0ed26cd0eb4f6d4511f5
Betojr04 <betomasia12@gmail.com>
2023-11-22 08:48:11 -0700

    adjusted typos

--- src/views/Resume.js
+++ src/views/Resume.js
@@ -3,9 +3,6 @@ import React from "react";
 export const Resume = () => {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      {/* <a href="/Resume.pdf" download>
-        Download My Resume
-      </a> */}
       <iframe
         src="/Resume.pdf"
         title="My Resume"
