86f50d978f8853992eb80b4dd7609db1ec5bac8a
Betojr04 <betomasia12@gmail.com>
2024-03-11 09:29:58 -0700

    commented out barber and persoal fitness projects since I removed the repos from my github in exchange for different projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Project.css";
-import barber from "../assets/barber.png";
-import fitness from "../assets/fitness.png";
+// import barber from "../assets/barber.png";
+// import fitness from "../assets/fitness.png";
 
 export const Projects = () => {
   const projectData = [
-    {
-      title: "Barber",
-      description: "Website for barbers using React and EmailJS, ",
-      link: "https://barber-lake-nine.vercel.app/",
-      iframesrc: "https://barber-lake-nine.vercel.app/",
-      image: barber,
-    },
-    {
-      title: "Personal Trainer",
-      description: "Website for personal trainers using ReactJS, and EmailJS.",
-      link: "https://personal-training-web.vercel.app/",
-      iframesrc: "https://personal-training-web.vercel.app/",
-      image: fitness,
-    },
+    // {
+    //   title: "Barber",
+    //   description: "Website for barbers using React and EmailJS, ",
+    //   link: "https://barber-lake-nine.vercel.app/",
+    //   iframesrc: "https://barber-lake-nine.vercel.app/",
+    //   image: barber,
+    // },
+    // {
+    //   title: "Personal Trainer",
+    //   description: "Website for personal trainers using ReactJS, and EmailJS.",
+    //   link: "https://personal-training-web.vercel.app/",
+    //   iframesrc: "https://personal-training-web.vercel.app/",
+    //   image: fitness,
+    // },
     {
       title: "FinanceBuddy",
       description:
         "Take control of your money with this Fulll-Stack AI-powered personal finance app. Connect your accounts with ease using Plaid API, and watch as our AI suggests smart places to put your money. It offers personalized financial habits, backed by data from your transactions, bills, and more. Our AI is your financial educator, simplifying complex concepts. Got questions? Ask away! Plus, enjoy modern financial reports and a dashboard for all your insights. Financial freedom starts here.",
       link: "https://github.com/Betojr04/FinanceBuddy",
       iframesrc: "https://github.com/Betojr04/FinanceBuddy",
-      image: "https://i.imgur.com/4K0VX6n.png",
+      image: "https://i.imgur.com/4K0VX6n.png"
     },
     {
       title: "Nomad",
@@ -34,7 +34,7 @@ export const Projects = () => {
         "Developing a web-based platform for creating, managing, and sharing travel itineraries. Designing and implementing a RESTful API with Flask, integrating secure JWT authentication and SQLAlchemy for database management. Crafting a responsive front-end using React and Redux, offering features like itinerary planning, social sharing, and real-time updates.",
       link: "https://github.com/Betojr04/nomad",
       iframesrc: "https://github.com/Betojr04/nomad",
-      image: "https://i.imgur.com/4K0VX6n.png",
+      image: "https://i.imgur.com/4K0VX6n.png"
     },
     {
       title: "Personal Finance Tracker",
@@ -42,8 +42,8 @@ export const Projects = () => {
         "Tracker for your personal finances, creat accounts, update balances, and view transaction. Generate reports on your spending habits, and view your net worth. Save your data between sessions and include authentication for security. Devling into this project to learn the ins and outs of Java before converting it into a full stack spingboot project",
       iframesrc: "https://github.com/Betojr04/personalFinanceTracker",
       image: "https://i.imgur.com/4K0VX6n.png",
-      link: "https://github.com/Betojr04/personalFinanceTracker",
-    },
+      link: "https://github.com/Betojr04/personalFinanceTracker"
+    }
   ];
   return (
     <section className="projects-section" id="projects">
